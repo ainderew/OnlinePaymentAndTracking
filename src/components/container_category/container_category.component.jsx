743cb959e56972ecec2aa76bc2ba8db3ced610ea
Andrew Pinon <andrewapinon@gmail.com>
2022-01-28 18:54:10 +0800

    changed category container colors (removed getColor function on container)

--- src/components/container_category/container_category.component.jsx
+++ src/components/container_category/container_category.component.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./container_category.style.scss";
 
-import {getColor} from "../../scripts/DOM";
+import { getColor } from "../../scripts/DOM";
 
-const getItems = () =>{
-    
-}
 
-const Component_Container_Category = ({ParentData, index, getItemFetchFunction}) =>{
-    return(
-        <div onClick={()=>{getItemFetchFunction(ParentData.id)}} style={{backgroundColor: "#75B6FD"}} className="container_category">
+const Component_Container_Category = ({ ParentData, index, getItemFetchFunction }) => {
+    return (
+        <div onClick={() => { getItemFetchFunction(ParentData.id) }}
+            // style={{ color: getColor(index) }} 
+            className="container_category">
             <span className="header_container">{ParentData.name}</span>
             <span className="span_details">10 Items</span>
         </div>
